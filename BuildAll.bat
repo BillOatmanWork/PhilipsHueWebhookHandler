3f907b98700972586c20a2872113374d763448b2
Bill Oatman <billoatman0380@gmail.com>
2025-04-03 14:34:35 -0400

    More release related changes

--- BuildAll.bat
+++ BuildAll.bat
@@ -5,21 +5,21 @@ dotnet publish -r linux-arm64 -c Release /p:PublishSingleFile=true /p:IncludeNat
 dotnet publish -r linux-x64 -c Release /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true --self-contained
 
 
-cd C:\repos\PDSportsNet8\Build
+cd C:\repos\PhilipsHueWebhookHandler\Build
 
-copy /Y "C:\repos\PDSportsNet8\bin\Release\net8.0\win-x64\publish\PDSports.exe" .
-"C:\Program Files\7-Zip\7z" a -tzip PDSports-WIN-8.zip PDSports.exe SampleFavorites.json "MoneyLine Odds Explained.txt" PDSports.pdf
+copy /Y "C:\repos\PhilipsHueWebhookHandler\bin\Release\net8.0\win-x64\publish\PhilipsHueWebhookHandler.exe" .
+"C:\Program Files\7-Zip\7z" a -tzip PhilipsHueWebhookHandler-WIN-8.zip PhilipsHueWebhookHandler.exe SampleConfig.json PhilipsHueWebhookHandler.pdf
 
-copy /Y "C:\repos\PDSportsNet8\bin\Release\net8.0\osx-x64\publish\PDSports" .
-"C:\Program Files\7-Zip\7z" a -t7z PDSports-OSX-8.7z PDSports SampleFavorites.json "MoneyLine Odds Explained.txt" PDSports.pdf
+copy /Y "C:\repos\PhilipsHueWebhookHandler\bin\Release\net8.0\osx-x64\publish\PhilipsHueWebhookHandler" .
+"C:\Program Files\7-Zip\7z" a -t7z PhilipsHueWebhookHandler-OSX-8.7z PhilipsHueWebhookHandler SampleConfig.json PhilipsHueWebhookHandler.pdf
 
-copy /Y "C:\repos\PDSportsNet8\bin\Release\net8.0\linux-x64\publish\PDSports" .
-"C:\Program Files\7-Zip\7z" a -t7z PDSports-LIN64-8.7z PDSports SampleFavorites.json "MoneyLine Odds Explained.txt" PDSports.pdf
+copy /Y "C:\repos\PhilipsHueWebhookHandler\bin\Release\net8.0\linux-x64\publish\PhilipsHueWebhookHandler" .
+"C:\Program Files\7-Zip\7z" a -t7z PhilipsHueWebhookHandler-LIN64-8.7z PhilipsHueWebhookHandler SampleConfig.json PhilipsHueWebhookHandler.pdf
 
-copy /Y "C:\repos\PDSportsNet8\bin\Release\net8.0\linux-arm\publish\PDSports" .
-"C:\Program Files\7-Zip\7z" a -t7z PDSports-RasPi-8.7z PDSports SampleFavorites.json "MoneyLine Odds Explained.txt" PDSports.pdf
+copy /Y "C:\repos\PhilipsHueWebhookHandler\bin\Release\net8.0\linux-arm\publish\PhilipsHueWebhookHandler" .
+"C:\Program Files\7-Zip\7z" a -t7z PhilipsHueWebhookHandler-RasPi-8.7z PhilipsHueWebhookHandler SampleConfig.json PhilipsHueWebhookHandler.pdf
 
-copy /Y "C:\repos\PDSportsNet8\bin\Release\net8.0\linux-arm64\publish\PDSports" .
-"C:\Program Files\7-Zip\7z" a -t7z PDSports-RasPi64-8.7z PDSports SampleFavorites.json "MoneyLine Odds Explained.txt" PDSports.pdf
+copy /Y "C:\repos\PhilipsHueWebhookHandler\bin\Release\net8.0\linux-arm64\publish\PhilipsHueWebhookHandler" .
+"C:\Program Files\7-Zip\7z" a -t7z PhilipsHueWebhookHandler-RasPi64-8.7z PhilipsHueWebhookHandler SampleConfig.json PhilipsHueWebhookHandler.pdf
 
 
